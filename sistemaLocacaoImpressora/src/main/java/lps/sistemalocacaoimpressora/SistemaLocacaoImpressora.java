94ec2ac715b763ed1bcafc9793dd6a2bf4a35d12
Gabriel <gabriellopescarvalho@live.com>
2022-12-02 09:23:39 -0300

    Sprint01 - Cadastros - Adiciondo os cadastros do sistema

--- sistemaLocacaoImpressora/src/main/java/lps/sistemalocacaoimpressora/SistemaLocacaoImpressora.java
+++ sistemaLocacaoImpressora/src/main/java/lps/sistemalocacaoimpressora/SistemaLocacaoImpressora.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package lps.sistemalocacaoimpressora;
+
+import lps.sistemalocacaoimpressora.view.FrMenu;
 
 public class SistemaLocacaoImpressora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrMenu tela = new FrMenu();
+        tela.setVisible(true);
     }
 }
